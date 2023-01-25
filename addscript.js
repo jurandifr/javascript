7f34017fd2d115fd365ad3db07cd912a1ed804f2
JURANDI <60081684+jurandi82@users.noreply.github.com>
2023-01-25 12:08:11 -0300

    Adicionado linha do html2canvas

--- addscript.js
+++ addscript.js
@@ -8,3 +8,11 @@
      document.head.appendChild(script);
  }
  addScript('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js');
+
+ // Para copiar trechos como imagem
+
+ addScript('https://html2canvas.hertzen.com/dist/html2canvas.min.js');
+
+ html2canvas(document.querySelector("#book-holder")).then(canvas => {
+    navigator.clipboard.write(canvas)
+ });
